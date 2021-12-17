c2604b06375967a5ddd53a1e98bd134505271ba1
ohnguyen2404 <ohn24042000@gmail.com>
2021-12-17 21:57:18 +0700

    Add redux-persist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/styles/global.scss';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App';
 import {Provider} from 'react-redux'
-import {store} from './store'
+import {store, persistor} from './store'
 
 ReactDOM.render(
     <Provider store={store}>
-        <React.StrictMode>
-            <App/>
-        </React.StrictMode>
+        <PersistGate loading={null} persistor={persistor}>
+            <React.StrictMode>
+                <App/>
+            </React.StrictMode>
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,26 @@
-import {applyMiddleware, createStore} from "redux";
-import {logger} from "redux-logger";
-import {composeWithDevTools} from "redux-devtools-extension";
+import { applyMiddleware, createStore } from "redux";
+import { logger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-const middleware = [thunk];
-if (process.env.REACT_APP_NODE_ENV === 'dev') {
-    middleware.push(logger);
+const persistConfig = {
+  key: "root",
+  storage,
 }
 
-export const store = createStore(rootReducer,
-    composeWithDevTools(applyMiddleware(...middleware)));
+const middleware = [thunk];
+if (process.env.REACT_APP_NODE_ENV === "dev") {
+  middleware.push(logger);
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export const persistor = persistStore(store)
