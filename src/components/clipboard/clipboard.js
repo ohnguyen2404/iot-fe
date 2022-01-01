4c794316a9f44fff04fc0409430934ebc8371054
ohnguyen2404 <ohn24042000@gmail.com>
2022-01-01 11:54:23 +0700

    Copy to clipboard deviceId and credentials

---
+++ src/components/clipboard/clipboard.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import copy from 'copy-to-clipboard'
+import {
+  Button,
+  message,
+} from "antd";
+
+const Clipboard = (props) => {
+  const {label, copyText, ...restProps} = props
+  const handleCopy = () => {
+    copy(copyText)
+    message.success(`${copyText} has been copied to clipboard`)
+  }
+
+  return (
+    <Button
+      className={restProps.className}
+      icon="copy"
+      type="primary" 
+      onClick={handleCopy}>
+      {label}
+    </Button>
+  )
+}
+
+export default Clipboard
--- src/components/device/InfoDeviceModal.js
+++ src/components/device/InfoDeviceModal.js
@@ -10,14 +10,16 @@ import {
   message,
   Tabs,
   Cascader,
+  Row,
+  Col,
 } from "antd";
 import constant from "../../helpers/constants";
+import Clipboard from "../clipboard/clipboard";
 import { DeviceService } from "../../services";
 import { get } from "lodash";
 import ManageCredentials from "../device-credentials/ManageCredentials";
 
 const { TabPane } = Tabs;
-
 
 const InfoDeviceModal = (props) => {
   const { openInfoModal, handleOpenModal, deviceId } = props;
@@ -72,6 +74,29 @@ const InfoDeviceModal = (props) => {
 
   const _deviceName = get(deviceInfo, "device.name");
   const _deviceLabel = get(deviceInfo, "device.label");
+  const _credentialsType = get(deviceInfo, "deviceCredentials.credentialsType");
+  const _credentialsId = get(deviceInfo, "deviceCredentials.credentialsId");
+  const _credentialsValue = get(
+    deviceInfo,
+    "deviceCredentials.credentialsValue"
+  );
+
+  let clipBoardText, clipBoardLabel;
+  const isTypeAccessToken =
+    _credentialsType === constant.DEVICE_CREDENTIALS_TYPE_ACCESS_TOKEN;
+  const isTypeX509 =
+    _credentialsType === constant.DEVICE_CREDENTIALS_TYPE_X_509;
+  const isTypeMqttBasic =
+    _credentialsType === constant.DEVICE_CREDENTIALS_TYPE_MQTT_BASIC;
+
+  if (isTypeAccessToken) {
+    clipBoardLabel = "Copy access token";
+    clipBoardText = _credentialsId;
+  }
+  if (isTypeMqttBasic) {
+    clipBoardLabel = "Copy MQTT credentials";
+    clipBoardText = _credentialsValue;
+  }
 
   return (
     <div>
@@ -90,11 +115,29 @@ const InfoDeviceModal = (props) => {
         cancelButtonProps={styleButton}
         centered={true}
       >
-        <Button 
-        type="primary"
-        onClick={() => handleOpenCredentialsModal(true)}>
-          Manage Credentials
-        </Button>
+        <div>
+          <Button
+            icon="safety-certificate"
+            type="primary"
+            onClick={() => handleOpenCredentialsModal(true)}
+          >
+            Manage Credentials
+          </Button>
+        </div>
+        <br />
+        <div>
+          <Clipboard
+            label="Copy Device ID"
+            copyText={deviceId}
+          />
+          {!isTypeX509 && (
+            <Clipboard
+              label={clipBoardLabel}
+              copyText={clipBoardText}
+              className="m-l-5"
+            />
+          )}
+        </div>
         <Form className="edit_device_form" layout="vertical">
           <Tabs defaultActiveKey="1">
             <TabPane tab="Device Details" key="1">
